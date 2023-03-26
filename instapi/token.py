0b1d6933261fa88223cabef8188c18aded5c5ca4
Mael Guillossou <mael.guillossou@gmail.com>
2023-03-26 20:03:32 +0200

    add data folder

--- instapi/token.py
+++ instapi/token.py
@@ -1,8 +1,8 @@
 def save_token(token):
-    with open("token.txt", "w") as f:
+    with open("data/token.txt", "w") as f:
         f.write(token)
 
 def load_token():
-    with open("token.txt", "r") as f:
+    with open("data/token.txt", "r") as f:
         token = f.readline()
     return token
